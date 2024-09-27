0f2155253058d97015d0c4b1ef38e5014eb9354b
S.Lazarakis <socratislazarakis@gmail.com>
2024-09-27 16:06:40 +0300

    1

---
+++ src/test/java/com/example/AppTest.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class AppTest {
+
+    @Test
+    public void testGreet() {
+        App app = new App();
+        String result = app.greet("World");
+        assertEquals("Hello, World!", result);
+    }
+}
